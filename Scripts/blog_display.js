8dda6d4c86760fbde1f67377d26b139f2e20ab68
YoavLippi <131270199+YoavLippi@users.noreply.github.com>
2024-04-24 19:50:26 +0200

    Added scripting for menu display; Overhauled blog display

---
+++ Scripts/blog_display.js
@@ -0,0 +1,52 @@
+//NOT USING NODE.JS FOR NOW, SO EACH FILEPATH MUST BE ADDED MANUALLY
+let file_names = [
+    "/Blogs/Blog 1.txt",
+]
+
+const button_names = [
+    "The Rise of AI",
+    "Design Wireframes and Plans",
+    "Semantic Markup and More Plans",
+    "IxD and Design",
+]
+
+function initialise_buttons() {
+    const button_holder = document.getElementById("button_holder");
+    button_names.forEach(function (bName, index) {
+        let newButton = document.createElement("button");
+        newButton.innerText = bName;
+        newButton.addEventListener('click', () => {
+                read_entry(`/Blogs/Blog ${index + 1}.txt`);
+        });
+        button_holder.appendChild(newButton);
+    });
+}
+
+export function initialise() {
+    initialise_buttons();
+    read_entry(file_names[0]);
+}
+
+export function read_entry(filepath) {
+    // console.log("called read entry")
+    // let output ="";
+    // const main = document.querySelector("body > main")
+    // const [file] = filepath.files;
+    // console.log(file)
+    // if (filepath.files && filepath.files[0]) {
+    //     console.log(filepath.files[0])
+    // }
+
+    fetch (filepath)
+        .then(response => response.text())
+        .then(text => checker(text))
+}
+
+//from the async function, we now have the string from the inputted file path
+function checker(file_text) {
+    //console.log(`We now have ${file_text}`)
+    const main = document.querySelector("body > main");
+    const artAnchor = document.createElement("article");
+    artAnchor.innerHTML = file_text;
+    main.replaceChildren(artAnchor)
+}
---
+++ Scripts/menu.js
@@ -0,0 +1,46 @@
+/*<li>Home</li>
+            <li><a href="blog.html">Blog</a></li>
+            <li><a href="essays.html">Essays</a></li>
+            <li><a href="portfolio.html">Portfolio</a></li>
+            <li><a href="design.html">Design</a></li>*/
+//todo Place the rest of the menu items
+const menuItems = [
+    { name: "Home", href: "index.html" },
+    { name: "Blog", href: "blog.html" },
+    { name: "Essays", href: "essays.html" },
+    { name: "Portfolio", href: "portfolio.html" },
+    { name: "Design", href: "design.html" }
+]
+
+export function initialise(currentPage = "") {
+    if (!(typeof currentPage === 'string' || currentPage instanceof String)) return;
+    const title = document.querySelector("header > h1")
+    // Title styling here in the future
+    const titleAnchor = document.createElement("a")
+    titleAnchor.innerText = "2024 WSOA3028A"
+    titleAnchor.setAttribute("id", "Top")
+
+    title.appendChild(titleAnchor)
+    const nav = document.querySelector("header > nav")
+    const ul = document.createElement("ul")
+
+    //populating list of pages with entries from menuItems array
+    for (let menuItem of menuItems) {
+        const li = document.createElement("li")
+        if (currentPage != menuItem.name) {
+            const a = document.createElement("a")
+            a.innerText = menuItem.name
+            a.setAttribute("href", menuItem.href)
+            li.appendChild(a)
+        } else {
+            const a = document.createElement("a")
+            a.innerText = menuItem.name
+            li.appendChild(a)
+        }
+        ul.appendChild(li)
+    }
+
+    nav.appendChild(ul)
+}
+
+//initialise()
