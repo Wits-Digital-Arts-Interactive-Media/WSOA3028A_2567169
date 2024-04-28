de204a11f8ec560b6ed79551a447d3caa696a5c7
YoavLippi <131270199+YoavLippi@users.noreply.github.com>
2024-04-28 15:02:21 +0200

    Added new javascript functionality for expanding buttons with summaries Overhauled pdf functionality to allow tiling and downloading

--- Scripts/blog_display.js
+++ Scripts/blog_display.js
@@ -2,20 +2,37 @@ const button_names = [
     "The Rise of AI",
     "Design Wireframes and Plans",
     "Semantic Markup and More Plans",
-    "IxD and Design",
+    "IxD Design",
 ]
+
+let isOpenArr = [];
 
 const root = "/WSOA3028A_2567169";
 
 function initialise_buttons() {
     const button_holder = document.getElementById("button_holder");
     button_names.forEach(function (bName, index) {
+        isOpenArr.push(false);
         let newButton = document.createElement("button");
+        newButton.id = `blog_button_${index}`
         newButton.innerText = bName;
         newButton.addEventListener('click', () => {
-                read_entry(`${root}/blogs/Blog ${index + 1}.txt`);
+            read_entry(`${root}/blogs/Blog ${index + 1}.txt`, index);
         });
+
+        newButton.addEventListener('mouseover', () => {
+            read_summary(`${root}/blogs/Blog-${index + 1}-Summary.txt`, index);
+        });
+
+        newButton.addEventListener('mouseleave', () => {
+            clear_text(index);
+        });
+
         button_holder.appendChild(newButton);
+
+        let article_holder = document.createElement("article");
+        article_holder.id = `Blog_Holder_${index}`;
+        button_holder.appendChild(article_holder);
     });
 }
 
@@ -24,26 +41,45 @@ export function initialise() {
     //read_entry(`${root}/blogs/Blog 1.txt`);
 }
 
-export function read_entry(filepath) {
-    // console.log("called read entry")
-    // let output ="";
-    // const main = document.querySelector("body > main")
-    // const [file] = filepath.files;
-    // console.log(file)
-    // if (filepath.files && filepath.files[0]) {
-    //     console.log(filepath.files[0])
-    // }
-
-    fetch (filepath)
-        .then(response => response.text())
-        .then(text => checker(text))
+function clear_text(index) {
+    if (isOpenArr[index]) return;
+    const blog_holder = document.getElementById(`Blog_Holder_${index}`);
+    blog_holder.innerHTML = "";
 }
 
+//async function, executes when it can find the file at the specified filepath
+function read_entry(filepath, index) {
+    fetch (filepath)
+        .then(response => response.text())
+        .then(text => checker(text, index))
+}
+
+function read_summary(filepath, index) {
+    if (isOpenArr[index]) return;
+    fetch (filepath)
+        .then(response => response.text())
+        .then(text => summary_display(text, index))
+}
+
+function summary_display(summary_text, index) {
+    const blog_holder = document.getElementById(`Blog_Holder_${index}`);
+    blog_holder.innerHTML = summary_text;
+}
 //from the async function, we now have the string from the inputted file path
-function checker(file_text) {
+function checker(file_text, index) {
     //console.log(`We now have ${file_text}`)
-    const main = document.querySelector("body > main");
-    const artAnchor = document.createElement("article");
-    artAnchor.innerHTML = file_text;
-    main.replaceChildren(artAnchor)
+    // const main = document.querySelector("body > main");
+    // const artAnchor = document.createElement("article");
+    // artAnchor.innerHTML = file_text;
+    // main.replaceChildren(artAnchor)
+    const blog_holder = document.getElementById(`Blog_Holder_${index}`);
+    let corresponding_button = document.getElementById(`blog_button_${index}`)
+    if (isOpenArr[index]) {
+        blog_holder.innerHTML = "";
+        corresponding_button.innerText = button_names[index];
+    } else {
+        blog_holder.innerHTML = file_text;
+        corresponding_button.innerText = "Close";
+    }
+    isOpenArr[index] = !isOpenArr[index];
 }
--- Scripts/essay_display.js
+++ Scripts/essay_display.js
@@ -1,14 +1,22 @@
-export function expand_pdf(pdf_path) {
+let isOpenArr = [];
+
+export function expand_pdf(pdf_path, article_location, array_check_location) {
     //<iframe style="width: 1300px; height: 2000px;" src="Essays/Essay 1 UI and UX.pdf"></iframe>
-    const articleHolder = document.getElementById("pdf_holder");
-    const pdf_frame = document.createElement("iframe");
-    pdf_frame.setAttribute('style', "width: 1300px; height: 2000px;")
-    pdf_frame.setAttribute('src', pdf_path);
-    articleHolder.replaceChildren(pdf_frame);
+    const articleHolder = document.getElementById(article_location);
+    if (isOpenArr[array_check_location]) {
+        articleHolder.replaceChildren("")
+    } else {
+        const pdf_frame = document.createElement("iframe");
+        pdf_frame.setAttribute('style', "width: 1300px; height: 2000px;")
+        pdf_frame.setAttribute('src', pdf_path);
+        articleHolder.replaceChildren(pdf_frame);
+    }
+    isOpenArr[array_check_location] = !isOpenArr[array_check_location];
 }
 
 let button_names = [
     'Essay 1: UI and UX',
+    'Tiling Tester',
 ]
 
 const root = "/WSOA3028A_2567169";
@@ -18,10 +26,21 @@ export function initialise() {
     button_names.forEach(function (bName, index) {
         let newButton = document.createElement("button");
         newButton.innerText = bName;
+        isOpenArr.push(false)
         newButton.addEventListener('click', () => {
-            expand_pdf(`${root}/essays/Essay-${index+1}.pdf`);
+            expand_pdf(`${root}/essays/Essay-${index+1}.pdf`, `pdf_holder_${index+1}`, index);
         })
+
         button_holder.appendChild(newButton);
+        let expand_button = document.createElement("button");
+        expand_button.innerHTML = `<a href="${root}/essays/Essay-${index+1}.pdf" download>Download Essay ${index+1}</a>`;
+
+        let article_with_id = document.createElement("article");
+        article_with_id.id = `pdf_holder_${index+1}`;
+
+        button_holder.appendChild(expand_button);
+        button_holder.appendChild(document.createElement("br"));
+        button_holder.appendChild(article_with_id);
     })
 }
 
