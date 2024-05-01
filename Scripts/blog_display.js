f1496f0ba9f404e723865e529f7de65e41389131
YoavLippi <131270199+YoavLippi@users.noreply.github.com>
2024-05-01 14:58:33 +0200

    Finished design page, added mobile handling and migrated all blogs from word to txt

--- Scripts/blog_display.js
+++ Scripts/blog_display.js
@@ -1,65 +1,106 @@
+import { is_mobile } from "./mobile_handling.js";
+
+//const holder so mobile checker method is only called once
+const is_mobile_check = is_mobile(navigator.userAgent || navigator.vendor || window.opera);
+
+//Global scope for access from multiple functions
 const button_names = [
-    "Blog 1: The Rise of AI\nKeywords: AI, Turing Test, Twitter",
-    "Blog 2: Design Wireframes and Plans\nKeywords: Design, Art, Wireframes",
-    "Blog 3: Semantic Markup and More Plans\nKeywords: Semantic Markup, Website Design, html5",
-    "Blog 4: IxD Design\nKeywords: IxD, Design, Semantic Markup",
-]
+    "Blog 1: The Rise of AI",
+    "Blog 2: Design Wireframes and Plans",
+    "Blog 3: Semantic Markup and More Plans",
+    "Blog 4: IxD Design",
+    "Blog 5: Sites for UI and UX analysis",
+    "Blog 6: Racial Justice, Gender Equality, and the Internet",
+    "Blog 7: Web Dev as a South African",
+    "Blog 8: A Close Reading on a Feminist Internet",
+];
 
-let isOpenArr = [];
+const button_keywords = [
+    'AI, Turing Test, Twitter',
+    'Design, Art, Wireframes',
+    'Semantic Markup, Website Design, html5',
+    'IxD, Design, Semantic Markup',
+    'UI, UX, IxD, Design Analysis',
+    'Social Justice, Internet Access, Systemic Bias, AI',
+    'Internet Access, Systemic Bias, Human Rights',
+    'Feminism, Gender equality, Social justice, Social media',
+];
 
-const root = "/WSOA3028A_2567169";
+let is_open_arr = [];
 
-function initialise_buttons() {
-    const button_holder = document.getElementById("button_holder");
-    button_names.forEach(function (bName, index) {
-        isOpenArr.push(false);
-        let newButton = document.createElement("button");
-        newButton.id = `blog_button_${index}`
-        newButton.innerText = bName;
-        newButton.addEventListener('click', () => {
-            read_entry(`${root}/blogs/Blog ${index + 1}.txt`, index);
-        });
-
-        newButton.addEventListener('mouseover', () => {
-            read_summary(`${root}/blogs/Blog-${index + 1}-Summary.txt`, index);
-        });
-
-        newButton.addEventListener('mouseleave', () => {
-            clear_text(index);
-        });
-
-        button_holder.appendChild(newButton);
-
-        let article_holder = document.createElement("article");
-        article_holder.id = `Blog_Holder_${index}`;
-        article_holder.classList.add("h-entry")
-        button_holder.appendChild(article_holder);
-    });
-}
-
+//right now only redirects, but can have other functionality added later
 export function initialise() {
     initialise_buttons();
     //read_entry(`${root}/blogs/Blog 1.txt`);
 }
 
+function initialise_buttons() {
+    const root = "/WSOA3028A_2567169";
+    //required to have an element with this ID
+    const button_holder = document.getElementById("button_holder");
+
+    const nav_bar = document.getElementById('blog_jumper');
+    const button_menu = document.createElement("menu");
+    nav_bar.appendChild(button_menu);
+
+    button_names.forEach(function (bName, index) {
+        is_open_arr.push(false);
+        let new_button = document.createElement("button");
+        new_button.id = `blog_button_${index}`;
+        new_button.innerText = `${bName}${(index<button_keywords.length)? `\nKeywords:${button_keywords[index]}` : ``}`;
+
+        // Add event listeners for each button after they have been created and appended to the DOM
+        // This approach ensures that event listeners are added only after the buttons are available in the DOM,
+        // avoiding null reference errors that may occur when trying to access elements before they are created.
+        new_button.addEventListener('click', function (e) {
+            read_entry(`${root}/blogs/blog-richtexts/Blog ${index + 1}.txt`, index);
+        });
+
+        if (!is_mobile_check) {
+            new_button.addEventListener('mouseover', function (e) {
+                read_summary(`${root}/blogs/blog-summaries/Blog-${index + 1}-Summary.txt`, index);
+            });
+    
+            new_button.addEventListener('mouseleave', function (e) {
+                clear_text(index);
+            });
+        }
+
+        button_holder.appendChild(new_button);
+
+        let article_holder = document.createElement("article");
+        article_holder.id = `Blog_Holder_${index}`;
+        article_holder.classList.add("h-entry");
+        button_holder.appendChild(article_holder);
+
+        let corresponding_nav = document.createElement("li");
+        let nav_anchor = document.createElement("a");
+        nav_anchor.innerText = `${bName}`;
+        nav_anchor.href = `#${article_holder.id}`;
+        nav_anchor.rel = "bookmark";
+        corresponding_nav.appendChild(nav_anchor);
+        button_menu.appendChild(corresponding_nav);
+    });
+}
+
 function clear_text(index) {
-    if (isOpenArr[index]) return;
+    if (is_open_arr[index]) return;
     const blog_holder = document.getElementById(`Blog_Holder_${index}`);
     blog_holder.innerHTML = "";
 }
 
 //async function, executes when it can find the file at the specified filepath
 function read_entry(filepath, index) {
-    fetch (filepath)
+    fetch(filepath)
         .then(response => response.text())
-        .then(text => checker(text, index))
+        .then(text => checker(text, index));
 }
 
 function read_summary(filepath, index) {
-    if (isOpenArr[index]) return;
-    fetch (filepath)
+    if (is_open_arr[index]) return;
+    fetch(filepath)
         .then(response => response.text())
-        .then(text => summary_display(text, index))
+        .then(text => summary_display(text, index));
 }
 
 function summary_display(summary_text, index) {
@@ -70,18 +111,16 @@ function summary_display(summary_text, index) {
 //from the async function, we now have the string from the inputted file path
 function checker(file_text, index) {
     //console.log(`We now have ${file_text}`)
-    // const main = document.querySelector("body > main");
-    // const artAnchor = document.createElement("article");
-    // artAnchor.innerHTML = file_text;
-    // main.replaceChildren(artAnchor)
+
+    //TODO: Fix issue of mouseover events breaking when the user gets pushed upwards due to a blog being collapsed?
     const blog_holder = document.getElementById(`Blog_Holder_${index}`);
-    let corresponding_button = document.getElementById(`blog_button_${index}`)
-    if (isOpenArr[index]) {
+    let corresponding_button = document.getElementById(`blog_button_${index}`);
+    if (is_open_arr[index]) {
         blog_holder.innerHTML = "";
-        corresponding_button.innerText = button_names[index];
+        corresponding_button.innerText = `${button_names[index]}${(index<button_keywords.length)? `\nKeywords:${button_keywords[index]}` : ``}`;
     } else {
-        blog_holder.innerHTML = file_text;
+        blog_holder.innerHTML = file_text + `<nav style="text-align: center;"><a href = "#Top">Jump to top</a></nav>`;
         corresponding_button.innerText = `Close ${button_names[index]}`;
     }
-    isOpenArr[index] = !isOpenArr[index];
+    is_open_arr[index] = !is_open_arr[index];
 }
--- Scripts/menu.js
+++ Scripts/menu.js
@@ -1,49 +1,43 @@
-/*<li>Home</li>
-            <li><a href="blog.html">Blog</a></li>
-            <li><a href="essays.html">Essays</a></li>
-            <li><a href="portfolio.html">Portfolio</a></li>
-            <li><a href="design.html">Design</a></li>*/
-//todo Place the rest of the menu items
-
 const root = "/WSOA3028A_2567169";
 
-const menuItems = [
+const menu_items = [
     { name: "Home", href: `${root}/index.html` },
     { name: "Blog", href: `${root}/blogs/index.html` },
     { name: "Essays", href: `${root}/essays/index.html` },
     { name: "Portfolio", href: `${root}/portfolio/index.html` },
     { name: "Design", href: `${root}/design/index.html` }
-]
+];
 
 export function initialise(currentPage = "") {
     if (!(typeof currentPage === 'string' || currentPage instanceof String)) return;
-    const title = document.querySelector("header > h1")
+    const title = document.querySelector("header > h1");
     // Title styling here in the future
-    const titleAnchor = document.createElement("a")
-    titleAnchor.innerText = "2024 WSOA3028A"
-    titleAnchor.setAttribute("id", "Top")
+    const title_anchor = document.createElement("a");
+    title_anchor.innerText = "2024 WSOA3028A";
+    title_anchor.setAttribute("id", "Top");
 
-    title.appendChild(titleAnchor)
-    const nav = document.querySelector("header > nav")
-    const ul = document.createElement("ul")
+    title.appendChild(title_anchor);
+    const nav = document.querySelector("header > nav");
+    //Changed from ul to menu because it is more semantically accurate
+    const ul = document.createElement("menu");
 
     //populating list of pages with entries from menuItems array
-    for (let menuItem of menuItems) {
-        const li = document.createElement("li")
+    for (let menuItem of menu_items) {
+        const li = document.createElement("li");
         if (currentPage != menuItem.name) {
-            const a = document.createElement("a")
-            a.innerText = menuItem.name
-            a.setAttribute("href", menuItem.href)
-            li.appendChild(a)
+            const a = document.createElement("a");
+            a.innerText = menuItem.name;
+            a.setAttribute("href", menuItem.href);
+            li.appendChild(a);
         } else {
-            const a = document.createElement("a")
-            a.innerText = menuItem.name
-            li.appendChild(a)
+            const a = document.createElement("a");
+            a.innerText = menuItem.name;
+            li.appendChild(a);
         }
-        ul.appendChild(li)
+        ul.appendChild(li);
     }
 
-    nav.appendChild(ul)
+    nav.appendChild(ul);
 }
 
 //initialise()
