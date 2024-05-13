f29ee3c2a76380fae340c99cb70b56b7d72e9b61
YoavLippi <131270199+YoavLippi@users.noreply.github.com>
2024-05-13 23:33:56 +0200

    Fixed targeting issue for unvisited links, changed let declarations in essay initialise function to const

--- Scripts/essay_display.js
+++ Scripts/essay_display.js
@@ -2,6 +2,7 @@ let is_open_arr = [];
 
 let button_names = [
     'Essay 1: UI and UX',
+    'Essay 2: Untitled',
     'Tiling Tester',
 ];
 
@@ -24,27 +25,32 @@ function expand_pdf(pdf_path, article_location, array_check_location) {
 export function initialise() {
     const button_holder = document.getElementById("buttons");
     button_names.forEach(function (bName, index) {
-        let new_button = document.createElement("button");
+        const new_button = document.createElement("button");
         new_button.classList.add("bordered_entry");
         new_button.innerText = bName;
         is_open_arr.push(false)
         //DOMContentLoaded may be called before all buttons have been added, so listeners are registered here instead
         new_button.addEventListener('click', function (e) {
             expand_pdf(`${root}/essays/Essay-${index + 1}.pdf`, `pdf_holder_${index + 1}`, index);
-        })
+        });
 
         button_holder.appendChild(new_button);
 
         //FOR ASSIGNMENT SUBMISSION, QUESTION: for the future, is this button needed? There is a download button in some viewers but I'm not sure if it's global
-        let download_button = document.createElement("button");
-        download_button.innerHTML = `<a href="${root}/essays/Essay-${index + 1}.pdf" download>Download Essay ${index + 1}</a>`;
+        const download_button = document.createElement("button");
+        const download_anchor = document.createElement("a");
+        download_anchor.href = `${root}/essays/Essay-${index + 1}.pdf`;
+        download_anchor.download = `${bName}_2567169`;
+        download_anchor.innerText = `Download Essay ${index+1}`;
+        download_button.appendChild(download_anchor);
+        //download_button.innerHTML = `<a href="${root}/essays/Essay-${index + 1}.pdf" download>Download Essay ${index + 1}</a>`;
         download_button.classList.add("bordered_entry");
 
-        let article_with_id = document.createElement("article");
+        const article_with_id = document.createElement("article");
         article_with_id.id = `pdf_holder_${index + 1}`;
 
         button_holder.appendChild(download_button);
         button_holder.appendChild(document.createElement("br"));
         button_holder.appendChild(article_with_id);
-    })
+    });
 }
