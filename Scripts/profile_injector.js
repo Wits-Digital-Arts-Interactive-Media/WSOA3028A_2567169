fe532d358ddc9ca2f6c6878bf0affbf7acb9dd29
YoavLippi <131270199+YoavLippi@users.noreply.github.com>
2024-05-13 22:28:46 +0200

    Added CSS styling for links Added functionality for removing coloring in the mailto link in the profile

--- Scripts/profile_injector.js
+++ Scripts/profile_injector.js
@@ -3,17 +3,38 @@ let profile_rich_html =
     <h1 class = "profile_header">Profile:_</h1>
     <p class = "h-card">
         <img class="pfp u-photo" src="/WSOA3028A_2567169/portfolio/images/Self_portrait.png" alt="A self portrait of Yoav" title="Yoav Lipshitz">
-        <span class="item"><span class="dBlue">Name</span> : <span class="lBlue p-name">Yoav Lipshitz</span></span>
-        <a class="item" href="mailto:2567169@students.wits.ac.za"><span class="dBlue u-email">Email</span> : <span class="lBlue">2567169@students.wits.ac.za</span></a>
-        <span class="item"><span class="dBlue">Location</span> : <span class="lBlue p-org">University of the Witwatersrand</span></span>
-        <span class="item"><span class="dBlue">City</span> : <span class="lBlue p-locality">Johannesburg</span></span>
-        <span class="item"><span class="dBlue">Country</span> : <span class="lBlue p-country-name">South Africa</span></span>
+        <span class="item"><span class="dBlue">Name</span>&nbsp;:&nbsp;<span class="lBlue p-name">Yoav Lipshitz</span></span>
+        <a class="item" id = "mailto_link" href="mailto:2567169@students.wits.ac.za"><span class="dBlue u-email">Email</span>&nbsp;:&nbsp;<span class="lBlue">2567169@students.wits.ac.za</span></a>
+        <span class="item"><span class="dBlue">Location</span>&nbsp;:&nbsp;<span class="lBlue p-org">University of the Witwatersrand</span></span>
+        <span class="item"><span class="dBlue">City</span>&nbsp;:&nbsp;<span class="lBlue p-locality">Johannesburg</span></span>
+        <span class="item"><span class="dBlue">Country</span>&nbsp;:&nbsp;<span class="lBlue p-country-name">South Africa</span></span>
     </p>
     </div>`;
 
 function inject_profile(location_id) {
     const container = document.getElementById(location_id);
     container.innerHTML = profile_rich_html;
+
+    //Adding a listener to remove the coloring spans on mouse over and put them back when the mouse leaves on the mailto link
+    const mailto_link = document.getElementById('mailto_link');
+    //console.log(`mailto link is ${mailto_link.tagName}`)
+    mailto_link.addEventListener('mouseenter', function (e) {
+        remove_spans(mailto_link);
+    });
+    mailto_link.addEventListener('mouseleave', function (e) {
+        add_spans(mailto_link);
+    });
+}
+
+//Explicitly referencing each part of the link because this is a tailored function, it cannot be used elsewhere
+function remove_spans(mailto_link = document.createElement('a')) {
+    mailto_link.children[0].className = "";
+    mailto_link.children[1].className = "p-name";
+}
+
+function add_spans(mailto_link = document.createElement('a')) {
+    mailto_link.children[0].className = "dBlue";
+    mailto_link.children[1].className = "lBlue p-name";
 }
 
 //racer function
