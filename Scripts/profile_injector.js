5b5f5449c6d4f9948121147b673bf0079bf1ab80
YoavLippi <131270199+YoavLippi@users.noreply.github.com>
2024-05-15 20:48:11 +0200

    Continued styling on home screen

--- Scripts/profile_injector.js
+++ Scripts/profile_injector.js
@@ -1,14 +1,16 @@
 let profile_rich_html =
     `<div class = 'container'>
-    <h1 class = "profile_header">Profile:_</h1>
-    <p class = "h-card">
-        <img class="pfp u-photo" src="/WSOA3028A_2567169/portfolio/images/Self_portrait.png" alt="A self portrait of Yoav" title="Yoav Lipshitz">
-        <span class="item"><span class="dBlue">Name</span>&nbsp;:&nbsp;<span class="lBlue p-name">Yoav Lipshitz</span></span>
-        <a class="item" id = "mailto_link" href="mailto:2567169@students.wits.ac.za"><span class="dBlue u-email">Email</span>&nbsp;:&nbsp;<span class="lBlue">2567169@students.wits.ac.za</span></a>
-        <span class="item"><span class="dBlue">Location</span>&nbsp;:&nbsp;<span class="lBlue p-org">University of the Witwatersrand</span></span>
-        <span class="item"><span class="dBlue">City</span>&nbsp;:&nbsp;<span class="lBlue p-locality">Johannesburg</span></span>
-        <span class="item"><span class="dBlue">Country</span>&nbsp;:&nbsp;<span class="lBlue p-country-name">South Africa</span></span>
-    </p>
+        <div class = "h-card profile_info">
+            <img class="pfp u-photo" src="/WSOA3028A_2567169/portfolio/images/Self_portrait.png" width=100vw alt="A self portrait of Yoav" title="Yoav Lipshitz">
+            <div class = "text_info">
+                <h1 class = "profile_header">Profile:_</h1>
+                <span class="item"><span class="dBlue">Name</span>&nbsp;:&nbsp;<span class="lBlue p-name">Yoav Lipshitz</span></span>
+                <a class="item" id = "mailto_link" href="mailto:2567169@students.wits.ac.za"><span class="dBlue u-email">Email</span>&nbsp;:&nbsp;<span class="lBlue">2567169@students.wits.ac.za</span></a>
+                <span class="item"><span class="dBlue">Location</span>&nbsp;:&nbsp;<span class="lBlue p-org">University of the Witwatersrand</span></span>
+                <span class="item"><span class="dBlue">City</span>&nbsp;:&nbsp;<span class="lBlue p-locality">Johannesburg</span></span>
+                <span class="item"><span class="dBlue">Country</span>&nbsp;:&nbsp;<span class="lBlue p-country-name">South Africa</span></span>
+            </div>
+        </div>
     </div>`;
 
 function inject_profile(location_id) {
@@ -37,11 +39,33 @@ function add_spans(mailto_link = document.createElement('a')) {
     mailto_link.children[1].className = "lBlue p-name";
 }
 
+function doDomSetup() {
+    inject_profile("profile_holder");
+    document.addEventListener('wheel', (e) => fadeElements(e))
+}
+
+function fadeElements(e) {
+    const profileHolder = document.getElementsByClassName('container')[0];
+    //document.documentElement.clientHeight + window.scrollY >= (document.documentElement.scrollHeight || document.documentElement.clientHeight)
+    let scrollTop = document.documentElement.clientHeight + window.scrollY;
+    console.clear();
+    console.log(scrollTop);
+
+    // Check if the user has scrolled down
+    if (e.wheelDeltaY > 0) {
+        profileHolder.style.opacity = 0.5;
+    } else {
+        profileHolder.style.opacity = 1;
+    }
+}
+
 //racer function
 if (document.readyState === "loading") {
     // If the document is still loading, add an event listener for DOMContentLoaded
-    document.addEventListener("DOMContentLoaded", inject_profile("profile_holder"));
+    document.addEventListener("DOMContentLoaded", doDomSetup());
 } else {
     // If the document has already loaded, call the function directly
-    inject_profile("profile_holder");
+    doDomSetup();
 }
+
+//window.onscroll = () => fadeElements();
